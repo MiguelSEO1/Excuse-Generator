b842266594d4d2fb0e781828d76bb577a37722ec
MiguelSEO1 <miguelsanlucarlebron@gmail.com>
2022-11-23 12:04:41 +0000

    excuse generator project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //array with the words
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // declaring random variables
+  let random1 = Math.floor(Math.random() * who.length);
+  let random2 = Math.floor(Math.random() * action.length);
+  let random3 = Math.floor(Math.random() * what.length);
+  let random4 = Math.floor(Math.random() * when.length);
+
+  //creating a sentence (the excuse)
+  document.querySelector("#excuse").innerHTML =
+    who[random1] + action[random2] + what[random3] + when[random4];
 };
